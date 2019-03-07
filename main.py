f56f91780b86fb2e057701c3ac29cd3f53b5a128
mister025 <mister.0025@yandex.ru>
2019-03-07 21:14:36 +0100

    Threading. Complex NES fixed

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ import numpy as np
 from nes import run_nes, run_nes_improved
 
 
-env = gym.make('Pendulum-v0')  # BallBalancerSim-v0 
+env_name = 'Pendulum-v0'  # BallBalancerSim-v0 
 # print(env.action_space.low, env.action_space.high)
 # print(env.observation_space.low, env.observation_space.high)
 # print(env.action_space.sample())
 
-run_nes(env)
+run_nes_improved(env_name)
 
 """
 for i_episode in range(3):
--- nes.py
+++ nes.py
@@ -1,22 +1,24 @@
 from scipy import linalg as sl
+import threading
+import gym
 import numpy as np
 np.random.seed(1)
 
 from nes_suppl import ACTIONS, ACTIONS_S, basis_functions, basis_functions_s
 
 
-def act(obs, weights):
-	global ACTIONS_S
+def act(obs, weights, actions):
 	basis_funcs = basis_functions_s(obs)  # change depending on the env
-	weights = weights.reshape((len(ACTIONS_S), -1))
+	weights = weights.reshape((len(actions), -1))
 
 	q_estimate = np.dot(weights, basis_funcs)
 	action_i = np.argmax(q_estimate, axis=0)
-	return  ACTIONS_S[action_i]
+	return  actions[action_i]
 
 
 # fitness function
-def f(env, params, render=False):
+def f(env_name, params, actions, rewards, i, render=False):
+	env = gym.make(env_name)
 	observation = env.reset()
 	rollout_reward = 0
 	done = False
@@ -24,36 +26,42 @@ def f(env, params, render=False):
 		if render:
 			env.render()
 		# depending on the current observation
-		action = act(observation, params)
+		action = act(observation, params, actions)
 		observation, reward, done, info = env.step(action)
 		rollout_reward += reward
 
-	return rollout_reward
+	# print('Worker reward: {}'.format(rollout_reward))
+	rewards[i] = rollout_reward
+	# return rollout_reward
 
 
 def compute_log_gradients(m, cov_matrix, params):
-	cov_matrix_inv = np.linalg.pinv(cov_matrix)
-	prms_mean = (params - m).T
+	cov_matrix_inv = np.linalg.inv(cov_matrix)
+	prms_mean = (params - m).reshape((m.size, 1))
+	# print(prms_mean.shape)
 
 	grad_m = np.dot(cov_matrix_inv, prms_mean)
-	grad_cov = np.dot(np.dot(np.dot(0.5 * cov_matrix_inv, prms_mean), prms_mean.T), cov_matrix_inv) - 0.5 * cov_matrix_inv
-
+	grad_cov = 0.5 * np.dot(np.dot(np.dot(cov_matrix_inv, prms_mean), prms_mean.T), cov_matrix_inv) - 0.5 * cov_matrix_inv
+	# print(np.dot(np.dot(cov_matrix_inv, prms_mean), prms_mean.T))
+	# print(np.dot(cov_matrix_inv, prms_mean).shape)
 	return grad_m.T, grad_cov
 
 
 def compute_target_gradient(grads_m, grads_cov, fit_f_values, population_n):
+	# print(grads_cov)
+	# print(grads_m)
 	target_grad_m = 1 / population_n * np.sum(grads_m * fit_f_values.reshape((population_n, 1)), axis=0)
 	target_grad_cov = 1 / population_n * np.sum(grads_cov * fit_f_values.reshape((population_n, 1, 1)), axis=0)
 	return target_grad_m, target_grad_cov
 
 
-def run_nes_improved(env):
+def run_nes_improved(env_name):
 	global ACTIONS_S
 	episodes_n = 15000 
-	w = np.random.randn(len(ACTIONS_S) * 10)  # number of basis functions
-	population_n = 20
+	w = np.random.randn(len(ACTIONS_S) * 9)  # number of basis functions
+	population_n = 24
 	cov_matrix = np.zeros((w.size, w.size))  # cov_matrix = a_matrix.T @ amatrix
-	np.fill_diagonal(cov_matrix, 0.1)
+	np.fill_diagonal(cov_matrix, 0.01)
 	learning_rate = 1e-2
 
 	for i in range(episodes_n):
@@ -62,25 +70,38 @@ def run_nes_improved(env):
 		log_grads_m = np.zeros((population_n, w.size))
 		log_grads_cov = np.zeros((population_n, w.size, w.size))
 
+		threads = []
 		for worker in range(population_n):
 			worker_noise = (noise[worker]).T
-			worker_params = w + (sl.sqrtm(cov_matrix) @ worker_noise).T
-			rewards[worker] = f(env, worker_params, i > 5000)
-			# print('Worker reward: {}'.format(rewards[worker]))
-
+			worker_params = w + (sl.sqrtm(cov_matrix) @ worker_noise).T  # np.sqrt(cov_matrix)
+			# rewards[worker] = f(env, worker_params, ACTIONS_S, i > 5000)
 			log_grads_m[worker], log_grads_cov[worker] = compute_log_gradients(w, cov_matrix, worker_params)
+
+			t = threading.Thread(target=f, args=(env_name, worker_params, ACTIONS_S, rewards, worker, False))
+			threads.append(t)
+			t.start()
+
+		for t in threads:
+			t.join()
 
 		print('Episode: {} | Mean reward : {}'.format(i, np.mean(rewards)))
 
 		target_grad_m, target_grad_cov = compute_target_gradient(log_grads_m, log_grads_cov, rewards, population_n)
+		# print('Gradients: ')
+		# print(target_grad_m)  # 0 ????
+		# print(target_grad_cov)
 		w = w + learning_rate * target_grad_m
 		cov_matrix = cov_matrix + learning_rate * target_grad_cov
+		# print('Mean: ')
+		# print(w)
+		# print('Covariance matrix: ')
+		# print(cov_matrix)
 
 
-def run_nes(env):
+def run_nes(env_name):
 	global ACTIONS_S
 	episodes_n = 15000 
-	w = np.random.randn(len(ACTIONS_S) * 10)  # number of basis functions
+	w = np.random.randn(len(ACTIONS_S) * 9)  # number of basis functions
 	population_n = 30
 	sigma = 0.1
 	learning_rate = 1e-2
@@ -89,11 +110,18 @@ def run_nes(env):
 		noise = np.random.randn(population_n, w.size)
 		rewards = np.zeros(population_n)
 
+		threads = []
 		for worker in range(population_n):
 			worker_noise = noise[worker]
 			worker_params = w + sigma * worker_noise
-			rewards[worker] = f(env, worker_params, i > 5000)
+			# rewards[worker] = f(env_name, worker_params, ACTIONS_S, i > 10000)
+			t = threading.Thread(target=f, args=(env_name, worker_params, ACTIONS_S, rewards, worker, False))
+			threads.append(t)
+			t.start()
+		for t in threads:
+			t.join()
 
+		print('Rewards: {}'.format(rewards))
 		print('Episode: {} | Mean reward : {}'.format(i, np.mean(rewards)))
 
 		normalized_rewards = (np.asarray(rewards) - np.mean(rewards)) / np.std(rewards)
